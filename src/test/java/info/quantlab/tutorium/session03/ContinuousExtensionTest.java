3422b79fc67448279128036da124a641c673aa4e
Roland Bachl <bachl@math.lmu.de>
2021-05-03 12:59:16 +0200

    Fixed error in ContinuousExtensionTest

--- src/test/java/info/quantlab/tutorium/session03/ContinuousExtensionTest.java
+++ src/test/java/info/quantlab/tutorium/session03/ContinuousExtensionTest.java
@@ -9,11 +9,15 @@ class ContinuousExtensionTest {
 
 	@Test
 	void testEvaluateFunction() {
-		double[] arguments = new double[] { -2 * Math.PI, -Math.PI, 0, Math.PI, 2 * Math.PI };
+		double[] arguments = new double[] { -2 * Math.PI, 0, 2 * Math.PI };
 
 		for (double arg : arguments) {
 			assertEquals(0, evaluateFunction(arg), 1E-16);
 		}
+
+		double target = 0.5 / Math.PI;
+		assertEquals(target, evaluateFunction(-Math.PI), 1E-16);
+		assertEquals(-target, evaluateFunction(Math.PI), 1E-16);
 	}
 
 }
